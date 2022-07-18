73ce50f853c79eb9b95f0b00d5253020e78302b7
Alex Kent <34221930+front-end-code@users.noreply.github.com>
2022-07-18 18:21:17 +0400

    Update MobileHeader.js

--- src/sections/header/MobileHeader.js
+++ src/sections/header/MobileHeader.js
@@ -15,12 +15,14 @@ const MobileHeader = () => {
                     { toggleMenu ? <CloseOutlinedIcon /> : <MenuOutlinedIcon /> }
                 </section>
             </section>
-            <nav className={`${toggleMenu ? "translate-x-0" : "-translate-x-60"} w-60 bg-blue-500 flex flex-col justify-center text-white absolute h-screen top-10 z-50 transition duration-700`}>
+            <section className={`${toggleMenu ? "translate-x-0" : "-translate-x-60"} w-60 bg-blue-500 flex flex-col justify-center text-white absolute h-screen top-10 z-50 transition duration-700`}>
+              <nav>
                 <a href='/' className="">main</a>
                 <a href='/about' className="">about</a>
                 <a href='/portfolio' className="">portfolio</a> 
                 <a href='/contact' className="">contact</a>
-            </nav>
+              </nav>
+            </section>
         </section>
     )
 };
